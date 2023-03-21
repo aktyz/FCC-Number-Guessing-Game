94d888a4079b667bcfe24ca9cdfdf2627efd55ec
Zyta <zyta.slowianska@gmail.com>
2023-03-21 16:14:35 +0100

    feat: Final DB update

--- number_guess.sh
+++ number_guess.sh
@@ -2,7 +2,7 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 GAME() {
-echo -e "\nSecret in function scope: $SECRET"
+echo -e "\nSecret: $SECRET"
 until [[ $GUESS == $SECRET ]]
 do
   read GUESS
@@ -28,21 +28,26 @@ echo -e "\nEnter your username:"
 read USERNAME
 # oos: check username length (additional requirement - out of scope (oos))
 USER_INFO=$($PSQL "SELECT * FROM users WHERE username='$USERNAME';")
-if [[ -z $USER_INFO ]] # if username doesn't exist
+if [[ -z $USER_INFO ]]
 then
   CREATE_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME');")
   echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
 else
-  echo "$USER_INFO" | while IFS="|" read USER_ID USERNAME GAMES_PLAYED BEST_GAME
+  echo "$USER_INFO" | while IFS="|" read USER_ID USERNAME GAMES_PLAYED BEST_GAME_DB
   do
-    echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+    echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME_DB guesses."
   done
 fi
-# game logic
-USER_INFO=$($PSQL "SELECT * FROM users WHERE username='$USERNAME';")
-SECRET=$(( RANDOM%1000+1 ))
-echo -e "\nSecret in Global scope: $SECRET"
-echo -e "\nGuess the secret number between 1 and 1000:"
+
+BEST_GAME_DB=$($PSQL "SELECT best_game FROM users WHERE username='$USERNAME';")
+GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE username='$USERNAME';")
 BEST_GAME=1
+SECRET=$(( RANDOM%1000+1 ))
+echo -e "\nGuess the secret number between 1 and 1000:"
 GAME
-# upload results of the game to the DB
+
+RESULTS=$($PSQL "UPDATE users SET games_played=($GAMES_PLAYED+1) WHERE username='$USERNAME';")
+if [[ ! $BEST_GAME_DB || $BEST_GAME < $BEST_GAME_DB ]]
+then
+  RESULTS=$($PSQL "UPDATE users SET best_game=$BEST_GAME WHERE username='$USERNAME' ;")
+fi
